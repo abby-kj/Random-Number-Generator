dbbe55de485bbcf211075bb115d1c0e1e9d04e75
abby <ab.jegathev@gmail.com>
2025-03-16 11:11:47 -0400

    Python file that generates random number between 1 and 100 was created

---
+++ random_number.py
@@ -0,0 +1,8 @@
+import random
+
+# Generate a random integer between 1 and 100
+random_number = random.randint(1, 100)
+
+# Output the random number
+print ("Welcome Ladies and Gentlemen to the Random Number Generator!!")
+print("Your Random Number Generated:", random_number)
